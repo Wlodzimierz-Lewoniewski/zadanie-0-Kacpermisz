bc7fc8e3e4db93a15a53369fbadb11545aad8760
kacpermisz <kacpermisz0@gmail.com>
2024-10-13 14:46:20 +0200

    Zad_0

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
+import re
+
 zdania = int(input("Podaj liczbę zdań: "))
 i = 0
 j = 0
@@ -16,7 +18,8 @@ while j < slowa:
     lista_s = []
     slowo = input("Podaj slowo numer "+str(j+1)+": ").lower()
     for i, zdanie in enumerate(lista_z):
-        count = zdanie.lower().split().count(slowo)
+        wyr = re.findall(r'\b\w+\b', zdanie.lower())
+        count = wyr.count(slowo)
         lista_s.append((i, count))
     sort = sorted(lista_s, key=lambda x: x[1], reverse=True)
     for index, count in sort:
