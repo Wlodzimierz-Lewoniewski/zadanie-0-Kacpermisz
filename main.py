4ab121c3a4d4f70e8449430d7cee0582254dd209
kacpermisz <kacpermisz0@gmail.com>
2024-10-13 14:30:26 +0200

    Zad_0

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 #Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
-wyraz=input()
+zdania = int(input("Podaj liczbę zdań: "))
+i = 0
+j = 0
+lista_z = []
 
-#W celu poprawnego działania kodu w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
-#To pozwoli na usunięcie spacji oraz innych "spacjopodobnych" znaków (tabulacja \t', przejście do nowej linii '\n' lub '\r' etc.) z "głowy" i "ogona" (lewej i prawej części wyrazu).
-wyraz=wyraz.strip()
+while i < zdania:
+    zdanie = input("Podaj zdanie numer "+str(i+1)+": ")
+    lista_z.append(zdanie)
+    i += 1
 
-#Wydruk na ekranie (w konsoli)
-print("Test")
-print ('Ten wyraz został wprowadzony:', wyraz)
+slowa = int(input("Podaj liczbę słów: "))
+
+while j < slowa:
+    lista_i = []
+    lista_s = []
+    slowo = input("Podaj slowo numer "+str(j+1)+": ").lower()
+    for i, zdanie in enumerate(lista_z):
+        count = zdanie.lower().split().count(slowo)
+        lista_s.append((i, count))
+    sort = sorted(lista_s, key=lambda x: x[1], reverse=True)
+    for index, count in sort:
+        if count > 0:
+            lista_i.append(index)
+    print(lista_i)
+    j += 1
